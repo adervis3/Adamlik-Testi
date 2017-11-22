897082e72ad36e21ec7bc47dccd6ae4e4fd2d8c5
Erkan Ceylan <erkanceylan@protonmail.com>
2017-11-22 21:49:30 +0100

    Add files via upload

---
+++ Adamlik/Admin/admin_footer.php
@@ -0,0 +1,36 @@
+</div>
+</div>
+
+
+<!-- jQuery -->
+<script src="Assets/vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="Assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="Assets/vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="Assets/vendor/raphael/raphael.min.js"></script>
+<script src="Assets/vendor/morrisjs/morris.min.js"></script>
+<script src="Assets/data/morris-data.js"></script>
+<!-- DataTables JavaScript -->
+<script src="Assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="Assets/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+<script src="Assets/vendor/datatables-responsive/dataTables.responsive.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="Assets/dist/js/sb-admin-2.js"></script>
+
+<script src="../Ajax/myajax.js"></script>
+<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+</script>
+</body>
+
+</html>
